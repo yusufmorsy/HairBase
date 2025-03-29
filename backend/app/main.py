643c7ce6da945796e7cc4fdff8ad262d5ece198d
Lenix Not Linux <113266208+Limited-Lenix-Account@users.noreply.github.com>
2025-03-29 03:11:18 -0600

    updated with groq server

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,51 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import os
+from groq import Groq
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import json
 
 app = FastAPI()
+load_dotenv()
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+if GROQ_API_KEY is None:
+    raise ValueError("Api key not set")
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# Allows for image path to be specified on api call
+class ImageRequest(BaseModel):
+   image: str # base64 encoded
+
+
+client = Groq(api_key=GROQ_API_KEY)
+
+
+#prompt
+@app.post("/groq-ocr")
+async def groq_api_call(request: ImageRequest):
+    try:
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": """ What is the brand name, product name, and product type (shampoo, conditioner)? Give me your response in JSON, without any additional formatting. For example, {"brand_name": "Dove", "product_name": "Lavender Plus", "product_type": "shampoo"}. Please respond in this exact format, but using the given image. """},
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{request.image}",
+                            },
+                        },
+                    ],
+                }
+            ],
+            model="llama-3.2-11b-vision-preview",
+        )
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing Groq request: {str(e)}")
+
+    result = chat_completion.choices[0].message.content
+    return json.loads(result)
