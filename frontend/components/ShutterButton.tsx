c81cab47a22c4731f9b1a89117696da9462dee90
Kylo33 <56988649+Kylo33@users.noreply.github.com>
2025-03-29 02:21:56 -0600

    Update scanner page

---
+++ frontend/components/ShutterButton.tsx
@@ -0,0 +1,42 @@
+import Feather from "@expo/vector-icons/Feather";
+import { Pressable, StyleSheet, View } from "react-native";
+
+type Props = {
+  onPress?: () => void;
+};
+
+export default function ShutterButton({ onPress }: Props) {
+  return (
+    <View style={styles.outerContainer}>
+      <Pressable style={styles.pressable} onPress={onPress}>
+        <View style={styles.innerContainer}>
+          <Feather name="camera" size={32} color="#/>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  outerContainer: {
+    position: "absolute",
+    alignContent: "center",
+    justifyContent: "center",
+    height: 64,
+    width: 64,
+    borderRadius: 32,
+    borderWidth: 4,
+    borderColor: "#fff",
+    padding: 4,
+    bottom: 16,
+    alignSelf: "center",
+  },
+  pressable: {
+    flex: 1,
+  },
+  innerContainer: {
+    backgroundColor: "#fff",
+    flex: 1,
+    borderRadius: 100,
+  },
+});
