6023ee8fe2060c7989e454bbe6ea5934c9343ce8
Lenix Not Linux <113266208+Limited-Lenix-Account@users.noreply.github.com>
2025-03-30 00:05:48 -0600

    tweak

--- backend/app/main.py
+++ backend/app/main.py
@@ -124,7 +124,7 @@ async def groq_api_call(request: ImageRequest):
     generated_search_query = chat_completion.choices[0].message.content
 
     q = json.loads(generated_search_query)
-    
+    best_search_query(query_list, len(query_list))
     query_list = q["found_text"].split(" ")
     for i in range(0, len(query_list)):
         q = search_db(" ".join(query_list))
@@ -134,7 +134,7 @@ async def groq_api_call(request: ImageRequest):
         else:
             return q
         
-    best_search_query(query_list, len(query_list))
+
 
     return search_db(q["found_text"])
 
